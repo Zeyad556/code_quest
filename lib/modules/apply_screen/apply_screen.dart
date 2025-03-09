f1a3a6d607b6fb64701b5548d1b7b59b5d0424e7
Zeyad Sayed <155989330+Zeyad556@users.noreply.github.com>
2025-03-09 02:31:59 +0200

    .

--- lib/modules/apply_screen/apply_screen.dart
+++ lib/modules/apply_screen/apply_screen.dart
@@ -22,8 +22,6 @@ class _ApplyScreenState extends State<ApplyScreen> {
           ],
           title:Padding(
             padding: const EdgeInsetsDirectional.only(top: 50.0),
-
-            
             child: Row(
               children: [
                 Text(
