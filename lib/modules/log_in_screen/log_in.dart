7709969a07c51ab1d55cafc986965c0886d9d61b
Zeyad Sayed <155989330+Zeyad556@users.noreply.github.com>
2025-03-20 15:39:05 +0200

    update

--- lib/modules/log_in_screen/log_in.dart
+++ lib/modules/log_in_screen/log_in.dart
@@ -27,185 +27,187 @@ class _LogInScreenState extends State<LogInScreen> {
     return Scaffold(
       appBar: AppBar(backgroundColor: Colors.white, toolbarHeight: 30.0),
       resizeToAvoidBottomInset: false,
-      body: Expanded(
-        child: SingleChildScrollView(
-          reverse: true,
-          child: Form(
-            key: formKey,
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Padding(
-                        padding: const EdgeInsetsDirectional.symmetric(
-                          horizontal: 40.0,
-                        ),
-                        child: Image(
-                          image: AssetImage('assets/images/woman_2_logo.png'),
-                          width: 300.0.w,
-                          height: 500.0.h,
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.topRight,
-                      child: Padding(
-                        padding: const EdgeInsetsDirectional.symmetric(
-                          horizontal: 30.0,
-                          vertical: 10.0,
-                        ),
-                        child: Image(
-                          image: AssetImage('assets/images/code_quest_logo.png'),
-                          width: 80.0.w,
-                          height: 100.0.h,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 230),
-                  child: Text(
-                    'Phone Number',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 16.0.sp,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 5.0.h),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(
-                    end: 20,
-                    start: 20,
-                  ),
-                  child: Container(
-                    width: 360.0.w,
-                    height: 40.0.h,
-                    decoration: BoxDecoration(color: Colors.grey[350]),
-                    child: defaultFormField(
-                      controller: phoneController,
-                      type: TextInputType.phone,
-                      lable: 'Enter your phone number',
-                      prefix: Icons.phone,
-                      validator: (value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Phone must not be empty';
-                        }
-                        return null;
-                      },
-                    ),
-                  ),
-                ),
-                SizedBox(height: 30.0.h),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 270),
-                  child: Text(
-                    'Password',
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 16.0.sp,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                SizedBox(height: 5.0.h),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(
-                    end: 20,
-                    start: 20,
-                  ),
-                  child: Container(
-                    width: 360.0.w,
-                    height: 40.0.h,
-                    decoration: BoxDecoration(
-                      color: Colors.grey[350], //el [] 3shan opacity
-                    ),
-                    child: defaultFormField(
-                      controller: passwordController,
-                      type: TextInputType.visiblePassword,
-                      suffix:
-                          isPassword
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                      isPassword: isPassword,
-                      validator: (String? value) {
-                        if (value == null || value.isEmpty) {
-                          return 'Password must not be empty';
-                        }
-                        return null;
-                      },
-                      lable: 'Enter your password',
-                      prefix: Icons.lock,
-                      sufixPressed: () {
-                        setState(() {
-                          isPassword = !isPassword;
-                        });
-                      },
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsetsDirectional.only(end: 210),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder:
-                              (context) => ForgetPasswordWithPhoneScreen(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      'Forget your password?',
-                      style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.grey[600],
-                      ),
-                    ),
-                  ),
-                ),
-                SizedBox(height: 10.0.h),
-                Column(
-                  children: [
-                    Container(
-                      width: 300.w,
-                      height: 50.h,
-                      child: MaterialButton(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(50.0),
-                        ),
-                        color: Colors.deepPurple,
-                        child: Text(
-                          'LOG IN',
-                          style: TextStyle(
-                            fontSize: 20.0.sp,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
+      body: Center(
+        child: Expanded(
+          child: SingleChildScrollView(
+            reverse: true,
+            child: Form(
+              key: formKey,
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.symmetric(
+                            horizontal: 40.0,
+                          ),
+                          child: Image(
+                            image: AssetImage('assets/images/woman_2_logo.png'),
+                            width: 300.0.w,
+                            height: 500.0.h,
                           ),
                         ),
-                        onPressed: () {
-                          if(formKey.currentState!.validate()){
-                            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+                      ),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: Padding(
+                          padding: const EdgeInsetsDirectional.symmetric(
+                            horizontal: 30.0,
+                            vertical: 10.0,
+                          ),
+                          child: Image(
+                            image: AssetImage('assets/images/code_quest_logo.png'),
+                            width: 80.0.w,
+                            height: 100.0.h,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 230),
+                    child: Text(
+                      'Phone Number',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: 16.0.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5.0.h),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(
+                      end: 20,
+                      start: 20,
+                    ),
+                    child: Container(
+                      width: 360.0.w,
+                      height: 40.0.h,
+                      decoration: BoxDecoration(color: Colors.grey[350]),
+                      child: defaultFormField(
+                        controller: phoneController,
+                        type: TextInputType.phone,
+                        lable: 'Enter your phone number',
+                        prefix: Icons.phone,
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Phone must not be empty';
                           }
+                          return null;
                         },
                       ),
                     ),
-                    SizedBox(height: 10.0.h),
-                Padding(
-                  padding: EdgeInsets.only(
-                    bottom: MediaQuery.of(context).viewInsets.bottom,
                   ),
-                ),
-              ],
-            ),
-          ]),
+                  SizedBox(height: 30.0.h),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 270),
+                    child: Text(
+                      'Password',
+                      style: TextStyle(
+                        color: Colors.grey[500],
+                        fontSize: 16.0.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 5.0.h),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(
+                      end: 20,
+                      start: 20,
+                    ),
+                    child: Container(
+                      width: 360.0.w,
+                      height: 40.0.h,
+                      decoration: BoxDecoration(
+                        color: Colors.grey[350], //el [] 3shan opacity
+                      ),
+                      child: defaultFormField(
+                        controller: passwordController,
+                        type: TextInputType.visiblePassword,
+                        suffix:
+                            isPassword
+                                ? Icons.visibility
+                                : Icons.visibility_off,
+                        isPassword: isPassword,
+                        validator: (String? value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Password must not be empty';
+                          }
+                          return null;
+                        },
+                        lable: 'Enter your password',
+                        prefix: Icons.lock,
+                        sufixPressed: () {
+                          setState(() {
+                            isPassword = !isPassword;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 210),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder:
+                                (context) => ForgetPasswordWithPhoneScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        'Forget your password?',
+                        style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          color: Colors.grey[600],
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0.h),
+                  Column(
+                    children: [
+                      Container(
+                        width: 300.w,
+                        height: 50.h,
+                        child: MaterialButton(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(50.0),
+                          ),
+                          color: Colors.deepPurple,
+                          child: Text(
+                            'LOG IN',
+                            style: TextStyle(
+                              fontSize: 20.0.sp,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onPressed: () {
+                            if(formKey.currentState!.validate()){
+                              Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeScreen()));
+                            }
+                          },
+                        ),
+                      ),
+                      SizedBox(height: 10.0.h),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                    ),
+                  ),
+                ],
+              ),
+            ]),
+          ),
         ),
-      ),
-    ));
+            ),
+      ));
   }
 }
