d87a337b058fed7cd85bc7008984ff6f1778c2a7
Zeyad Sayed <155989330+Zeyad556@users.noreply.github.com>
2025-04-11 13:21:32 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
       builder: (context, child) {
         return MaterialApp(debugShowCheckedModeBanner: false, home: child);
       },
-      child: CounterScreen(),
+      child: HomeScreen(),
     );
   }
 }
